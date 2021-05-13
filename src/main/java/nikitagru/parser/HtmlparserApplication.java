8116d6ab1eccafd2a1f59dc6b05b319df8012b40
nikitagru <grunix123@gmail.com>
2021-05-13 13:12:52 +0500

    Init

---
+++ src/main/java/nikitagru/parser/HtmlparserApplication.java
@@ -0,0 +1,13 @@
+package nikitagru.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlparserApplication.class, args);
+	}
+
+}
